2d4875b212d269fcff99fd309b2ed5aa5996217d
BlackFeniix <utkin.kolya2012@yandex.ru>
2019-06-11 23:46:47 +0300

    Доработка кода до рабочего состояния. Рабочий вариант фильтра Собеля

--- SobelFilterTask.cs
+++ SobelFilterTask.cs
@@ -6,31 +6,77 @@ namespace Recognizer
     {
         public static double[,] SobelFilter(double[,] g, double[,] sx)
         {
+            int deltax = sx.GetLength(0) / 2;
+            int deltay = sx.GetLength(1) / 2;
             var width = g.GetLength(0);
             var height = g.GetLength(1);
-            var sy= new double[sx.GetLength(1),sx.GetLength(0)];  //транспонированная матрица sx
-            for (int i = 0; i < sy.GetLength(0); i++)
+            
+            var sy = Transpose(sx);  //транспонированная матрица sx
+            
+            var result = new double[width, height];
+            for (var x = deltax; x < width - deltax; x++)
+                for (var y = deltay; y < height - deltay; y++)
+                {
+                    var neighbourhood = GetNeighbourhood(g, x, y, sx.GetLength(0));
+                    var gx = MultiplyMatrix(sx, neighbourhood);
+                    var gy = MultiplyMatrix(sy, neighbourhood);
+                    result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                }
+
+            if (width == 1 && height == 1)
             {
-                for (int j = 0; j < sy.GetLength(1); j++)
+                var neighbourhood = GetNeighbourhood(g, 0, 0, sx.GetLength(0));
+                var gx = MultiplyMatrix(sx, neighbourhood);
+                var gy = MultiplyMatrix(sy, neighbourhood);
+                result[0, 0] = Math.Sqrt(gx * gx + gy * gy);
+            }
+            return result;
+        }
+
+        public static double[,] Transpose(double[,] sx)   //транспонирование матрицы 
+        {
+            var sy = new double[sx.GetLength(0), sx.GetLength(1)];
+            for (var i = 0; i < sx.GetLength(0); i++)
+            {
+                for (var j = 0; j < sx.GetLength(1); j++)
                 {
                     sy[i, j] = sx[j, i];
                 }
             }
-            var result = new double[width, height];
-            for (var x = 1; x < width - 1; x++)
-                for (var y = 1; y < height - 1; y++)
+            return sy;
+        }
+
+        public static double[,] GetNeighbourhood(double[,] g, int x, int y, int size)  //получение окрестности точки [x,y]
+        {
+            var result = new double[size, size];
+            var delta = size / 2;
+            if (size == 1)
+            {
+                result[0,0]=g[x, y];
+                return result;
+            }
+            
+            for (var i = -delta; i < delta+1; i++)
+                for (var j = -delta; j < delta+1; j++)
                 {
-                    // Вместо этого кода должно быть поэлементное умножение матриц sx и полученной из неё sy на окрестность точки (x, y)
-					// Такая операция ещё называется свёрткой (Сonvolution)
-                    var gx = 
-                        -g[x - 1, y - 1] - 2 * g[x, y - 1] - g[x + 1, y - 1] 
-                        + g[x - 1, y + 1] + 2 * g[x, y + 1] + g[x + 1, y + 1];
-                    var gy = 
-                        -g[x - 1, y - 1] - 2 * g[x - 1, y] - g[x - 1, y + 1] 
-                        + g[x + 1, y - 1] + 2 * g[x + 1, y] + g[x + 1, y + 1];
-                    result[x, y] = Math.Sqrt(gx * gx + gy * gy);
+                    var num = g[x + i, y + j];
+                    result[i + delta, j + delta] = g[x + i, y + j];
                 }
             return result;
+        }
+
+        public static double MultiplyMatrix(double[,] sxy, double[,] neighbourhood)
+        {
+            var sum = 0d;
+            var size = neighbourhood.GetLength(0);
+            for (var i = 0; i < size; i++)
+            {
+                for (var j = 0; j < size; j++)
+                {
+                    sum += sxy[i, j] * neighbourhood[i, j];
+                }
+            }
+            return sum;
         }
     }
 }
